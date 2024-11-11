02a224bfcbe50d161bd9f4294d6a9eb0c00eb167
Felix Kouadio <silvannkouadio08@gmail.com>
2024-11-11 14:11:06 +0000

    Sauvegarde automatique

---
+++ Git_Auto_Save.py
@@ -0,0 +1,27 @@
+import subprocess
+import time 
+
+# Délai en secondes (30 minutes).
+INTERVAL = 30*60
+def Git_Push():
+    try:
+        # Ajoute tous les changements.
+        subprocess.run(["git", "add", "."], check=True)
+
+        # Commit avec un message de automatique.
+        commit_message = "Sauvegarde automatique"
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+
+        # Pousse vers le dépot github.
+        subprocess.run(["git", "push"], check=True)
+
+        print("Changement éffecté et poussé sur Git")
+    except subprocess.CalledProcessError as e:
+        print("Erreur lors de la sauvegarde sur Git:", e)
+
+
+if __name__ == "__main__":
+    print("Script de Sauvegarde automatique démarré...")
+    while True:
+        Git_Push()
+        time.sleep(INTERVAL)
