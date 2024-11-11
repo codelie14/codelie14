a023b32d9645d4438abceeae78bd20d765fe9603
Felix Kouadio <silvannkouadio08@gmail.com>
2024-11-11 14:19:35 +0000

    Sauvegarde automatique Version 2024-11-11 14:19:35.486555

--- Git_Auto_Save.py
+++ Git_Auto_Save.py
@@ -1,15 +1,16 @@
 import subprocess
 import time 
-
+import datetime as dt
 # Délai en secondes (30 minutes).
-INTERVAL = 30
+INTERVAL = 60
 def Git_Push():
     try:
         # Ajoute tous les changements.
         subprocess.run(["git", "add", "."], check=True)
 
         # Commit avec un message de automatique.
-        commit_message = "Sauvegarde automatique"
+        current_date_and_time = dt.datetime.now()
+        commit_message = "Sauvegarde automatique Version " + str(current_date_and_time)
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
         # Pousse vers le dépot github.
